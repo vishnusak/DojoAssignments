04f0c103b28ffc36f018e2ae0af6e964b3dc9020
vishnusak <vishnusak@gmail.com>
2016-06-09 11:12:24 -0700

    9-June Assignment - JS and Python

---
+++ 9-JUNE-2016_Assignment/python/isLoop.py
@@ -0,0 +1,83 @@
+import random as rd
+
+class Node(object):
+    def __init__(self, data):
+        self.val = data
+        self.next = None
+
+class sList(object):
+    def __init__(self):
+        self.__head = None
+
+    def make_loop(self, num_of_nodes, loop_node):
+        for i in range(num_of_nodes):
+            # newnode = Node(rd.randint(1, 10))
+            newnode = Node(10 * (i+1))
+
+            if not self.__head:
+                self.__head = newnode
+            else:
+                temp = self.__head
+                while temp.next:
+                    temp = temp.next
+                temp.next = newnode
+
+            if (i + 1) == loop_node:
+                loop_head = newnode
+
+        try:
+            newnode.next = loop_head
+        except:
+            newnode.next = None
+
+        return self.show_loop(num_of_nodes)
+
+    def show_loop(self, num):
+        result = []
+        temp = self.__head
+
+        for i in range(num * 2):
+            try:
+                result.append(temp.val)
+                temp = temp.next
+            except:
+                break
+
+        return result
+
+    def is_loop(self):
+        result = False
+        node_list = []
+        count = 0
+        temp = None
+
+        if self.__head:
+            node_list.append(self.__head)
+            count += 1
+            temp = self.__head.next
+
+            while temp:
+                if temp in node_list:
+                    result = [(node_list.index(temp) + 1), count]
+                    break
+                else:
+                    node_list.append(temp)
+                    count += 1
+                    temp = temp.next
+
+        return result
+
+if __name__ == "__main__":
+    myLoop = sList()
+    nodes = 12
+    loop_start = 5
+
+    print("The number of nodes is {}").format(nodes)
+    print("The place where looping starts is {}").format(loop_start)
+    print("The looped list is {}").format(myLoop.make_loop(nodes, loop_start))
+
+    isLoop = myLoop.is_loop()
+    if isLoop:
+        print("The list has a loop. The last node is {} and looping starts at node {}").format(isLoop[1], isLoop[0])
+    else:
+        print("The list doesn't have a loop")
